0d0e33da759dce1dd33a2f026ff8211694004298
Stefano Giancristofaro <StGcontents@users.noreply.github.com>
2015-04-18 20:26:04 +0200

    Update webServerV2.c

--- webServerV2.c
+++ webServerV2.c
@@ -24,10 +24,10 @@
 
 void logger(int type_info, char *str1, char *str2, int i);
 
-// struct that contains many information of thread
+// structure containing thread informations
 struct thread_struct{
 	pthread_t tid; // thread id
-	int fd; // file descriptor of connection managed
+	int fd; // managed connection file descriptor
 };
 
 pthread_cond_t cond_queue = PTHREAD_COND_INITIALIZER; // conditon for queue, static initialization
@@ -36,11 +36,11 @@ pthread_mutex_t queue_mtx = PTHREAD_MUTEX_INITIALIZER; // queue mutex, static in
 struct thread_struct *threads; // threads array
 
 int *queue;
-int i = 0; // number of thread at work
-int put, get; // head and tail of queue
+int i = 0; // number of working threads 
+int put, get; // head and tail of the queue
 
 
-/* This function calculates the correct time formatted for logging routine */
+/* This function calculates the correct time, formatted for logging routine */
 
 char *now(void)
 {
@@ -53,20 +53,20 @@ char *now(void)
 		exit(EXIT_FAILURE);
 	}
 	
-	// read the time using system call "time"
+	// read time using system call "time"
 	ticks = time(NULL); 
-    // write the time in buffer
+    // write time in buffer
     sprintf(buff, "%.24s", ctime(&ticks));
 
 	return buff;
 }
 
-/* This function implements the logging service and write into a log file the most important operations:
+/* This function implements the logging service and writes into a log file the most important operations:
  * - LOG_INFO: all correct operations
- * - NOT_FOUND: an operation that has requested a non present file
+ * - NOT_FOUND: an operation that has requested a not present file
  * - NOT_IMPLEMENTED: an operation that has requested a non supported method
  * - INTERNAL_ERROR: an operation that has generated an internal error 
- * - OTHER: the other internal error that not reguard client request */
+ * - OTHER: other internal errors that not concern client requests */
 
 void logger(int type_info, char *str1, char *str2, int i){
 	char log_buf[MAX_BUF*2];
@@ -115,38 +115,38 @@ ssize_t readn(int fd, void *buf, size_t n)
     if ((nread = read(fd, ptr, nleft)) < 0) {
       if (errno == EINTR)
         nread = 0;
-      // if timeout for socket in read (socket is blocking)  
+      // if timeout happens while socket is reading (socket is blocking)  
       if (errno == EWOULDBLOCK){
 		return -1; 
 	  }
       else
         return(-1);
     }
-    // if client close its socket 
+    // if client closes its socket 
     else if (nread == 0)
       break;	/* EOF */
 
     nleft -= nread;
     ptr += nread;
   }
-  return(n-nleft);	/* restituisce >= 0 */
+  return(n-nleft);	/* returns a positive value (>= 0) */
 }
 // end read function
 
 
-/* This function initialize the most important resource of server,
- * like thread's structure or queue for mange connection. 
- * Note that the synchronization resource like mutex and condition are initialized in static way */
+/* This function initializes the most important resources of server,
+ * like thread structure or queue for connection management. 
+ * Note that synchronization resources as mutex and condition are initialized in a static way */
 
 void initialize_resource(void){
 
-	// threads array initializzation
+	// threads array initialization
 	threads = malloc(sizeof(struct thread_struct)*MAX_THREAD);
 	if (threads == NULL){
 		logger(SYSTEM_ERR, SERVER_STRING, "Error in allocation memory for thread structure", (int)getpid());
 		exit(EXIT_FAILURE);
 	}
-	// end threads array initializzation
+	// end threads array initialization
 
 
 	// queue initalization
@@ -160,7 +160,7 @@ void initialize_resource(void){
 	// end queue initialization
 }
 
-/* This function locks a mutex for protect an area of memory shared between threads */
+/* This function locks a mutex to protect an area of memory shared between threads */
 void mutex_lock(pthread_mutex_t *mutex){
 	if (pthread_mutex_lock(mutex) != 0){
 		logger(SYSTEM_ERR, "Server", "Error in lock mutex semaphore", (int)getpid());
@@ -168,7 +168,7 @@ void mutex_lock(pthread_mutex_t *mutex){
 	}
 }
 
-/* This function release a mutex */
+/* This function releases a mutex */
 void mutex_unlock(pthread_mutex_t *mutex){
 	if (pthread_mutex_unlock(mutex) != 0){
 		logger(SYSTEM_ERR, "Server", "Error in unlock mutex semaphore", (int)getpid());
@@ -176,14 +176,14 @@ void mutex_unlock(pthread_mutex_t *mutex){
 	}
 }
 
-/* This function do the parse of client request; 
- * it finds the method request and path request (for file)*/
+/* This function parses the client request; 
+ * it finds requested method and file path*/
 
 int parse_request(char *req, char *path, char *type){
 	int i = 0;
 	int j = 0;
 	
-	// find the request method
+	// finds requested method
 	while(req[i] != ' '){
 		type[j] = req[i];
 		++i;
@@ -192,7 +192,7 @@ int parse_request(char *req, char *path, char *type){
 	
 	j = 0;
 	i = i+1;
-	// find the path of file requested
+	// finds requested file path 
 	while(req[i] != ' '){
 		path[j] = req[i];
 		++i;
@@ -202,8 +202,8 @@ int parse_request(char *req, char *path, char *type){
 	return j;
 }
 
-/* This function send 200 success code to client,
- * because the request is correct. Send the header to client */
+/* This function sends the 200 success code to client,
+ * because its request was successfully fulfilled. Sends header to client */
 
 void header_successful(int connfd, int i, ssize_t size){
 	char buf[MAX_BUF];
@@ -212,8 +212,8 @@ void header_successful(int connfd, int i, ssize_t size){
 	send(connfd, buf, strlen(buf), 0);
 	strcpy(buf, SERVER_STRING);
 	send(connfd, buf, strlen(buf), 0);
-	// Content_Lenght FONDAMENTALE PER PERSISTENZA PER EVITARE IL BLOCCO DEL CLIENT IN LETTURA
-	sprintf(buf, "Content-Length: %d\r\n", (int)size); // use sprintf for insert size param
+	// Content_Lenght FUNDAMENTAL FOR PERSISTENCY IMPLEMENTATION AND CLIENT LOCK AVOIDANCE DURING READING ROUTINE
+	sprintf(buf, "Content-Length: %d\r\n", (int)size); // uses sprintf to insert size param
 	send(connfd, buf, strlen(buf), 0);
 	if (i == 1){
 		strcpy(buf, "Content-Type: image/jpg\r\n");
@@ -229,8 +229,8 @@ void header_successful(int connfd, int i, ssize_t size){
 	send(connfd, buf, strlen(buf), 0);
 }
 
-/* This function send the only header to client,
- * beacause the request method was a HEAD request */
+/* This function sends only the header to the client,
+ * in response to a HEAD request */
 
 void head_request(int connfd){
 	char buf[MAX_BUF];
@@ -245,8 +245,8 @@ void head_request(int connfd){
 	send(connfd, buf, strlen(buf), 0);
 }
 
-/* This function send 404 error code to client,
- * because the request file is not found */
+/* This function sends the 404 error code to the client,
+ * because requested file was not found */
 
 void not_found(int connfd){
 	char buf[MAX_BUF];
@@ -272,7 +272,7 @@ void not_found(int connfd){
 	send(connfd, buf, strlen(buf), 0);
 }
 
-/* This function send 500 error code to client, 
+/* This function sends the 500 error code to the client, 
  * because an internal error occurred */
 
 void internal_error(int connfd){
@@ -295,7 +295,7 @@ void internal_error(int connfd){
 	send(connfd, buf, strlen(buf), 0);
 }
 
-/* This function send 501 error code to client, 
+/* This function sends the 501 error code to the client, 
  * because it has requested a method that is not implemented in the server */
 
 void not_implemented(int connfd){
@@ -318,13 +318,13 @@ void not_implemented(int connfd){
 	send(connfd, buf, strlen(buf), 0);
 }
 
-/* This function send the requested file to client using the function "sendfile" */
+/* This function sends the requested file to the client using "sendfile" procedure */
 
 void send_request_file(int connfd, int fd, char *path, pthread_t tid){
 	struct stat *buf;
 	int rc;	
 	
-	// alloc memory for stat structure
+	// memory allocation for stat structure
 	buf = (struct stat *)malloc(sizeof(struct stat));
 	if (buf == NULL){
 		logger(INTERNAL_ERROR, "Server", "Error in allocation memory for file statistic structure", (int)getpid());
@@ -332,7 +332,7 @@ void send_request_file(int connfd, int fd, char *path, pthread_t tid){
 		return;
 	}
 			
-	// take the information of the file
+	// takes informations from the file
 	if (fstat(fd, buf) == -1){
 		logger(INTERNAL_ERROR, "Server", "Error in fstat function for obtain file statistic", (int)getpid());
 		internal_error(connfd);
@@ -347,13 +347,13 @@ void send_request_file(int connfd, int fd, char *path, pthread_t tid){
 	else{
 		i = 0;
 	}
-	// send header HTTP to client
+	// sends HTTP header to the client
 	header_successful(connfd, i, buf->st_size);		
 	// write the file in the socket
 	rc = sendfile(connfd, fd, NULL, buf->st_size);
 	printf("Served by %u: ", (unsigned int)tid);
 
-	// error in write
+	// if errors occured during write procedure
 	if (rc == -1){
 		logger(INTERNAL_ERROR, "Server", "Error in write file to socket", (int)getpid());
 		internal_error(connfd);
@@ -369,7 +369,7 @@ void send_request_file(int connfd, int fd, char *path, pthread_t tid){
 	logger(LOG_INFO, "Server", "Successful send file", (int)getpid());	
 }
 
-/* This function parse and process the request send by client and controls the method */
+/* This function parses and processes the request send by the client to check the right method to call */
 void web_request(int connfd, pthread_t tid){
 	int fd;
 	ssize_t nread;
@@ -395,12 +395,12 @@ void web_request(int connfd, pthread_t tid){
 			return;
 		}
 		
-		// parse client request
+		// parses client request
 		parse_value = parse_request(req, path, type);
 		printf("HTTP Method: %s\n", type);
 		printf("File requested: %s\n", path);
 		
-		// verify if the request is a GET request
+		// verifies that the request is a GET request
 		if (strcmp(type, "GET") == 0){
 			if (parse_value == 1){
 				// the first page showed to user
@@ -421,7 +421,7 @@ void web_request(int connfd, pthread_t tid){
 			}
 		}
 		
-		// verify if the request is a HEAD request
+		// verifies that the request is a HEAD request
 		else if (strcmp(type, "HEAD") == 0){
 			// send only the header to client
 			head_request(connfd);
@@ -429,15 +429,15 @@ void web_request(int connfd, pthread_t tid){
 			continue;
 		}
 		
-		// otherwise send an error to client; other method are not implemented
+		// otherwise sends an error to the client; other methods are not implemented
 		else{
-			// send a server error to client
+			// sends a server error to the client
 			logger(NOT_IMPLEMENTED, "Server", "501 error code, method not implemented", (int)getpid());
 			not_implemented(connfd);
 			return;
 		}
 		
-		// send file to client
+		// sends file to the client
 		send_request_file(connfd, fd, path, tid);
 		
 		if (close(fd) == -1){
@@ -447,30 +447,30 @@ void web_request(int connfd, pthread_t tid){
 	}
 }
 
-/* This function is the starting point of the new threads when they are created from main thread 
- * here the thread take the first connection socket if queue is not empty; 
- * else it wait a condition from main thread (queue not empty) */
+/* This function is the starting point for newly generated threads (created by the main thread)
+ * here a thread takes its first connection socket if queue is not empty; 
+ * otherwise it waits for a condition update (queue not empty) from the main thread) */
 void *handle_conn(void *p){
-	// argument passed from pthread_create
+	// argument passed by pthread_create
 	struct thread_struct *thread = (struct thread_struct *)p;
 
 	while(1){
-		// mutual exclusion in queue (queue protected)
+		// mutual exclusion in queue (protected queue)
 		mutex_lock(&queue_mtx);
 		while (get == put){
 			fprintf(stderr, "Sleep\n");
-			// empty queue, wait condition  
+			// empty queue, waits for condition update
 			if (pthread_cond_wait(&cond_queue, &queue_mtx) != 0){
 				logger(SYSTEM_ERR, "Server", "Error in wait condition for empty queue", (int)getpid());
 				pthread_exit(NULL);
 			}
 		}
 		printf("Wake up thread %u\n", (unsigned int)pthread_self());
-		thread->fd = queue[get]; // take the first connection socket on list
+		thread->fd = queue[get]; // takes the first connection socket on the list
 		if (++get == MAX_THREAD){
 			get = 0;
 		}
-		++i; // thread busy	
+		++i; // busy thread 	
 		mutex_unlock(&queue_mtx);
 
 		// TODO work thread
@@ -484,12 +484,12 @@ void *handle_conn(void *p){
 			pthread_exit(NULL);
 		}
 		
-		// protect thread counter
+		// protected thread counter
 		mutex_lock(&queue_mtx);
-		--i; // thread is now free to work
+		--i; // thread is now allowed to work
 		mutex_unlock(&queue_mtx);
 
-		// signal to main thread: thread aviable for new connection
+		// signal sent to main thread: thread aviable for new connection
 		if (pthread_cond_signal(&cond_queue) != 0){
 			logger(SYSTEM_ERR, "Server", "Error in signal condition to main thread", (int)getpid());
             pthread_exit(NULL);
@@ -497,12 +497,12 @@ void *handle_conn(void *p){
 	}
 }
 
-/* This function create a pool of thread (MAX_THREAD) */
+/* This function creates a pool of MAX_THREAD threads */
 void make_threads(void){
 	int j;
 
 	for (j=0; j<MAX_THREAD; ++j){
-		// create a new thread
+		// creates a new thread
 		if (pthread_create(&(threads[j].tid), NULL, handle_conn, &(threads[j])) != 0){
 			logger(SYSTEM_ERR, "Server", "Error while create threads", (int)getpid());
 			exit(EXIT_FAILURE);
@@ -514,26 +514,26 @@ int start_server(void){
 	int sockfd;
 	struct sockaddr_in addr;
 
-	// create a TCP socket
+	// creates a TCP socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1){
 		logger(SYSTEM_ERR, "Server", "Error in open socket", (int)getpid());
 		return EXIT_FAILURE;
 	}
 
-	// clear sockaddr_in's memory
+	// clears sockaddr_in memory
 	memset((void *)&addr, 0, sizeof(addr));
 
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(PORT);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY); // accept connection from any IP address
+	addr.sin_addr.s_addr = htonl(INADDR_ANY); // accepts connection from any IP address
 
 	if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1){
 		logger(SYSTEM_ERR, "Server", "Error in bind socket", (int)getpid());
 		return EXIT_FAILURE;
 	}
 
-	// listen in main socket (listen socket)
+	// listen procedure in main socket (listen socket)
 	if (listen(sockfd, 0) == -1){
 		logger(SYSTEM_ERR, "Server", "Error in listen socket", (int)getpid());
 		return EXIT_FAILURE;
@@ -549,12 +549,12 @@ int main(void)
 	sockfd = start_server();
 	logger(LOG_INFO, "Start Server", "Work", (int)getpid());
 
-	// initialize all resources
+	// initializes all resources
 	initialize_resource();
-	// create pool threads
+	// creates pool of threads
 	make_threads();
 
-	// manage request
+	// manages request
 	for (;;){
 		if ((connfd = accept(sockfd, (struct sockaddr *)NULL, NULL)) == -1){
 			logger(SYSTEM_ERR, "Server", "Error in accept socket", (int)getpid());
@@ -562,17 +562,17 @@ int main(void)
 		}
 		
 		
-		// set a structure timeval for manage socket blocking
+		// sets a timeval structure to manage socket block
 		struct timeval tv;
 		tv.tv_sec = 20; 
 		tv.tv_usec = 0;
-		// set socket option in read
+		// sets socket option into read procedure
 		if (setsockopt(connfd, SOL_SOCKET, SO_RCVTIMEO,(char *)&tv,sizeof(struct timeval)) < 0){
 			logger(SYSTEM_ERR, "Server", "Error in set timeout for socket", (int)getpid());
 			return EXIT_FAILURE;
 		}
 
-		mutex_lock(&queue_mtx); // protect queue
+		mutex_lock(&queue_mtx); // protected queue
 		
 		while (i == MAX_THREAD){
 			fprintf(stderr, "Busy server...\n");
